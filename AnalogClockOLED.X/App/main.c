fb57c650af9254ea7f2b378449b898b7ed6d77bf
xpress-embedo <laboratory.embedded@gmail.com>
2020-05-25 23:07:26 +0200

    * Analog and Digital Display of Clock Done. * DS1307 submodule updated to include BCD to Decimal Converter Function * Readme to be updated.

--- AnalogClockOLED.X/App/main.c
+++ AnalogClockOLED.X/App/main.c
@@ -1,7 +1,7 @@
 /*
- * @file i2.c
+ * @file main.c
  * @author xpress_embedo
- * @date 1 Feb, 2020
+ * @date 24th May 2020
  * 
  * @brief Main File of the Project
  *
@@ -11,12 +11,18 @@
 #include "OLED.h"
 #include "ds1307.h"
 
+/* Project Related Macros */
+#define CLOCK_CENTER_X      ( 3u*OLED_Width()/4u )
+#define CLOCK_CENTER_Y      ( OLED_Height()/2u )
+
 /* Local Variables */
 static uint32_t millisecond = 0;
 
 /* Local Function */
 void timer0_init( void );
 void system_init( void );
+void display_clock_face( void );
+void display_time( uint8_t, uint8_t, uint8_t);
 
 
 /* Start From Here*/
@@ -36,6 +42,7 @@ void main(void)
       uint8_t year, month, date, hour, minute, second;
       static uint8_t sec_blink;
       timestamp = millis();
+      OLED_ClearDisplay();
       second = DS1307_Read (DS1307_SEC);
       minute = DS1307_Read (DS1307_MIN);
       hour   = DS1307_Read (DS1307_HOUR);
@@ -46,7 +53,7 @@ void main(void)
       if( sec_blink )
       {
         sec_blink = FALSE;
-        sprintf (lcd_msg, "TIME: %c%c:%c%c:%c%c", \
+        sprintf (lcd_msg, "%c%c:%c%c:%c%c", \
                  BCD2UpperCh (hour), BCD2LowerCh (hour), \
                  BCD2UpperCh (minute), BCD2LowerCh (minute), \
                  BCD2UpperCh (second), BCD2LowerCh (second) );
@@ -54,17 +61,24 @@ void main(void)
       else
       {
         sec_blink = TRUE;
-        sprintf (lcd_msg, "TIME: %c%c:%c%c %c%c", \
+        sprintf (lcd_msg, "%c%c:%c%c %c%c", \
                  BCD2UpperCh (hour), BCD2LowerCh (hour), \
                  BCD2UpperCh (minute), BCD2LowerCh (minute), \
                  BCD2UpperCh (second), BCD2LowerCh (second) );
       }
-      OLED_Write_Text (0, 0, lcd_msg);
-      sprintf (lcd_msg, "DATE: %c%c/%c%c/%c%c", \
+      OLED_Write_Text (0, 0, "  CLOCK");
+      OLED_Write_Text (4, 10, lcd_msg);
+      sprintf (lcd_msg, "%c%c/%c%c/%c%c", \
                BCD2UpperCh (date), BCD2LowerCh (date), \
                BCD2UpperCh (month), BCD2LowerCh (month), \
                BCD2UpperCh (year), BCD2LowerCh (year) );
-      OLED_Write_Text (0, 8, lcd_msg);
+      OLED_Write_Text (4, 18, lcd_msg);
+      
+      // Analog Clock
+      display_clock_face();
+      display_time( BCD2Decimal(hour), \
+                   BCD2Decimal(minute),\
+                   BCD2Decimal(second));
       OLED_Update ();
     }
   }
@@ -128,3 +142,66 @@ void __interrupt() ISR_ROUTINE( void )
   }
 }
 
+/**
+ * @brief Draw the Clock Face
+ */
+void display_clock_face( void )
+{
+  uint16_t idx = 0;
+  uint16_t x1, x2, y1, y2;
+  // Draw Clock Face
+//  for( idx = 0; idx<2u; idx++ )
+//  {
+//    OLED_Circle( CLOCK_CENTER_X, CLOCK_CENTER_Y, 31-idx, 1);  // Big Circle
+//  }
+//  for( idx=0; idx<3u; idx++ )
+//  {
+//    OLED_Circle( CLOCK_CENTER_X, CLOCK_CENTER_Y, idx, 1);     // Small Circle
+//  }
+  OLED_Circle( CLOCK_CENTER_X, CLOCK_CENTER_Y, 31, 1);  // Big Circle
+  OLED_Circle( CLOCK_CENTER_X, CLOCK_CENTER_Y, 2u, 1);  // Small Circle
+  // Draw Small Mark for Every Hour
+  // hour ticks
+  for( idx=0; idx<360; idx=idx+30 )
+  {
+    // Begin at 0 degree and stop at 360 degree
+    // To convert degree into radians, we have to multiply degree by (pi/180)
+    // degree (in radians) = degree * pi / 180
+    // degree (in radians) = degree / (180/pi)
+    // pi = 57.29577951 ( Python math.pi value )
+    float angle = idx ;
+    angle = (angle/57.29577951) ;         //Convert degrees to radians
+    x1 = (CLOCK_CENTER_X + (sin(angle)*31));
+    y1 = (CLOCK_CENTER_Y - (cos(angle)*31));
+    x2 = (CLOCK_CENTER_X + (sin(angle)*(31-5)));
+    y2 = (CLOCK_CENTER_Y - (cos(angle)*(31-5)));
+    OLED_Line(x1, y1, x2, y2, 1);
+  }
+}
+
+void display_time( uint8_t hour, uint8_t minute, uint8_t second)
+{
+  float angle = 0.0;
+  uint16_t temp = 0;
+  uint16_t x, y;
+  // display second hand
+  temp = (uint16_t)second * (uint16_t)6u;
+  angle= ((float)temp/57.29577951) ;      //Convert degrees to radians  
+  x = ( CLOCK_CENTER_X + (sin(angle)*(29)) );
+  y = ( CLOCK_CENTER_Y - (cos(angle)*(29)) );
+  OLED_Line( CLOCK_CENTER_X, CLOCK_CENTER_Y, x, y, 1);
+  
+  // display minute hand
+  temp = minute * 6u;
+  angle= ((float)temp/57.29577951) ;      //Convert degrees to radians  
+  x = ( CLOCK_CENTER_X + (sin(angle)*(22)) );
+  y = ( CLOCK_CENTER_Y - (cos(angle)*(22)) );
+  OLED_Line( CLOCK_CENTER_X, CLOCK_CENTER_Y, x, y, 1);
+  
+  // display hour hand
+  angle = hour * 30 + ((minute*6)/12 )   ;
+  angle=(angle/57.29577951) ;             //Convert degrees to radians  
+  x = ( CLOCK_CENTER_X + (sin(angle)*(15)) );
+  y = ( CLOCK_CENTER_Y - (cos(angle)*(15)) );
+  OLED_Line( CLOCK_CENTER_X, CLOCK_CENTER_Y, x, y, 1);
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 ## Analog Clock Based on OLED SSD1306 and PIC18F4550 Micro-controller
+
+![alt text](SIM/Digital_and_Analog_Clock.png "Working Simulation")
+
+### Steps to make an Analog Clock
+* Make a small circle of radius 1
+* Make a big circle of radius 31
+* Make Hour Ticks
+  * We have 12 Hours and 360 degrees, which means 1 Hour equals to 30 degrees.
+  * We will use a for loop incremented by 30 degrees, to draw ticks for each hour position.
+  * Now using sine and cosine formulas we will draw 12 marks of lenth 5 at each hour position.
+  * TODO:
